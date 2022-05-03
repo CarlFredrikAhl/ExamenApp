cd2497a61b48b1b1fc58e59d1f588addb4ceaf7a
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2022-05-03 10:52:31 +0200

    Fragments and navigation bar added

--- app/src/main/java/com/example/examenapp/MainActivity.java
+++ app/src/main/java/com/example/examenapp/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.examenapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    FitnessFragment fitnessFragment = new FitnessFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottonNav);
+
+        //Sets the container to home fragment which should be default
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.workout:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fitnessFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
     }
 }
