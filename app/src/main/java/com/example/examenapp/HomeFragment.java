88059c9cd356a4999f9c9e0a48e0cab2db2c2734
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2022-05-03 11:14:10 +0200

    Can click date

--- app/src/main/java/com/example/examenapp/HomeFragment.java
+++ app/src/main/java/com/example/examenapp/HomeFragment.java
@@ -2,11 +2,14 @@ package com.example.examenapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
@@ -15,6 +18,16 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        CalendarView calendarView = (CalendarView) view.findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                Toast.makeText(getActivity(), "Clicked Date", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/example/examenapp/MainActivity.java
+++ app/src/main/java/com/example/examenapp/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -16,12 +19,25 @@ public class MainActivity extends AppCompatActivity {
     HomeFragment homeFragment = new HomeFragment();
     FitnessFragment fitnessFragment = new FitnessFragment();
 
+    //CalendarView calendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottonNav);
+
+        //calendarView = findViewById(R.id.calendar);
+        //Toast.makeText(getApplicationContext(), (int) calendarView.getDate(), Toast.LENGTH_SHORT).show();
+        /*
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                Toast.makeText(getApplicationContext(), "Clicked Date", Toast.LENGTH_SHORT).show();
+            }
+        });
+         */
 
         //Sets the container to home fragment which should be default
         getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
