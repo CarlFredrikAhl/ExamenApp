02637bb9a9613fa67b29f2d3aa009ec26d892833
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2022-05-19 14:10:04 +0200

    Added statistics activity

--- app/src/main/java/com/example/examenapp/FitnessFragment.java
+++ app/src/main/java/com/example/examenapp/FitnessFragment.java
@@ -1,5 +1,6 @@
 package com.example.examenapp;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 public class FitnessFragment extends Fragment {
 
     ListView exercisesList;
+    ArrayList<String> exercises;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,10 +29,22 @@ public class FitnessFragment extends Fragment {
         exercisesList = view.findViewById(R.id.allExercisesList);
        //exercisesList.setBackgroundColor(Color.GRAY);
 
-        ArrayList<String> exercisesArray = Exercises.getAllExercises();
+        exercises = Exercises.getAllExercises();
+
+        ArrayList<String> exercisesArray = exercises;
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, exercisesArray);
         exercisesList.setAdapter(arrayAdapter);
+        exercisesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String exerciseName = adapterView.getItemAtPosition(i).toString();
+
+                Intent exerciseIntent = new Intent(getActivity(), StatisticsActivity.class);
+                exerciseIntent.putExtra("exercise_name", exerciseName);
+                startActivity(exerciseIntent);
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/example/examenapp/StatisticsActivity.java
+++ app/src/main/java/com/example/examenapp/StatisticsActivity.java
@@ -3,12 +3,22 @@ package com.example.examenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    String exerciseName;
+
+    TextView toolbarText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        exerciseName = getIntent().getStringExtra("exercise_name");
+
+        toolbarText = findViewById(R.id.toolbarText);
+        toolbarText.setText("Statistics - " + exerciseName);
     }
 }
