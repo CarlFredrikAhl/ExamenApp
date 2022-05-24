23d0575ff87de5461f52bde5bc37ff1cf39d4cf0
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2022-05-24 09:44:52 +0200

    Statistics chart works, need to replace with real data

--- app/src/main/java/com/example/examenapp/StatisticsActivity.java
+++ app/src/main/java/com/example/examenapp/StatisticsActivity.java
@@ -6,7 +6,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -33,8 +39,31 @@ public class StatisticsActivity extends AppCompatActivity {
 
         barEntryArrayList = new ArrayList<>();
         labelNames = new ArrayList<>();
+        labelNames.add("Vecka 1");
+        labelNames.add("Vecka 2");
+        labelNames.add("Vecka 3");
+        labelNames.add("Vecka 4");
+        labelNames.add("Vecka 5");
 
         barEntryArrayList = fillTestData();
+        BarDataSet  barDataSet = new BarDataSet(barEntryArrayList, "Test Data");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        Description desc = new Description();
+        desc.setText("I will replace this with real data later");
+        barChart.setDescription(desc);
+        BarData barData = new BarData(barDataSet);
+        barChart.setData(barData);
+
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(labelNames));
+        xAxis.setPosition(XAxis.XAxisPosition.TOP);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setGranularity(1f);
+        xAxis.setLabelCount(labelNames.size());
+        xAxis.setLabelRotationAngle(270);
+        barChart.animateY(2000);
+        barChart.invalidate();
 
         exerciseName = getIntent().getStringExtra("exercise_name");
 
@@ -54,7 +83,18 @@ public class StatisticsActivity extends AppCompatActivity {
     }
 
     private ArrayList<BarEntry> fillTestData() {
+        ArrayList<Float> testNumbers = new ArrayList<>();
+        testNumbers.add(10f);
+        testNumbers.add(20f);
+        testNumbers.add(30f);
+        testNumbers.add(40f);
+        testNumbers.add(50f);
+
         ArrayList<BarEntry> testDataArray = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++)  {
+            testDataArray.add(new BarEntry(i, testNumbers.get(i)));
+        }
 
         return testDataArray;
     }
