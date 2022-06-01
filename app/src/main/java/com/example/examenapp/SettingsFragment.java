95682805721d4ebca82e5b87486c6fe853ca4d75
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2022-06-01 14:00:47 +0200

    Started adding weight on settings fragment and end of 1 June

--- app/src/main/java/com/example/examenapp/SettingsFragment.java
+++ app/src/main/java/com/example/examenapp/SettingsFragment.java
@@ -1,7 +1,9 @@
 package com.example.examenapp;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
@@ -12,23 +14,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarEntry;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.sql.Array;
 import java.util.ArrayList;
 
 public class SettingsFragment extends Fragment {
 
     Button clearDataBtn;
+    Button doneBtn;
+
+    EditText weightPicker;
+
+    float startWeight;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        weightPicker = view.findViewById(R.id.weightPicker);
+        setWeight();
+
+        doneBtn = view.findViewById(R.id.doneBtn);
 
         clearDataBtn = view.findViewById(R.id.clearDataBtn);
         clearDataBtn.setOnClickListener(new View.OnClickListener() {
@@ -60,5 +76,46 @@ public class SettingsFragment extends Fragment {
         });
 
         return view;
+    }
+
+    public void setWeight() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("settings_data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor saveEditor = sharedPreferences.edit();
+
+        float savedWeight = sharedPreferences.getFloat("weight", 0f);
+        startWeight = savedWeight;
+
+        if(savedWeight != 0f) {
+            weightPicker.setText(String.valueOf(savedWeight) + " Kg");
+
+        } else {
+            weightPicker.setText("");
+        }
+    }
+
+    public void done() {
+        float weight = 0f;
+
+        try {
+            weight = Float.parseFloat(weightPicker.getText().toString());
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Insert your real weight", Toast.LENGTH_LONG).show();
+        }
+
+        if(weight > 20f) {
+
+        }
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("settings_data", Context.MODE_PRIVATE);
+        SharedPreferences.Editor saveEditor = sharedPreferences.edit();
+
+        float savedWeight = sharedPreferences.getFloat("weight", 0f);
+
+        if(savedWeight != 0f) {
+            weightPicker.setText(String.valueOf(savedWeight) + " Kg");
+
+        } else {
+            weightPicker.setText("");
+        }
     }
 }
